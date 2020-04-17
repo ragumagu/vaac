ba16a3d89f485dff866e43bed08f3db8548ce0a8
Shrinidhi <shrinidhihr98@gmail.com>
2020-04-17 13:26:30 +0530

    wrote csv analyzer

--- analyzer.py
+++ analyzer.py
@@ -15,88 +15,88 @@ from pathlib import Path
 # min_word_freq < word frequencies < max_word_freq
 
 # should write a summary file with all above data.
-class Analyze():
-    def __init__(self,files):
-        lines = []
-        for file in files:
-            lines.extend([item[0] for item in list(csv.reader(file))])
-        self.phrases = [phrase.split() for phrase in lines]
+
+
+class AnalyzeCSV():
+    def __init__(self, names):
+        self.sourcepaths = [f'./config/{name}.csv' for name in names]
+        if len(names) == 1:
+            self.sorted_path = f'./analytics/{names[0]}_sorted.csv'
+            self.counts_path = f'./analytics/{names[0]}_counts.csv'
+            self.partitions_path = f'./analytics/{names[0]}_partitions.csv'
+        else:
+            self.sorted_path = f'./analytics/sorted_summary.csv'
+            self.counts_path = f'./analytics/counts_summary.csv'
+            self.partitions_path = f'./analytics/partitions_summary.csv'
+
+        self.phrases = []
+        for source, name in zip(self.sourcepaths, names):
+            with open(source, 'r') as file:
+                lines = [item[0] for item in list(csv.reader(file))]
+                for line in lines:
+                    self.phrases.append([line.split(), name])
+
         self.count_words()
+        self.write_counts()
+        self.write_partitions()
         self.sort_phrases()
+        self.write_sorted()
 
     def count_words(self):
         '''stores word frequencies in self.counter'''
         self.counter = Counter()
         for phrase in self.phrases:
-            for word in phrase:
+            for word in phrase[0]:
                 self.counter[word] += 1
 
     def sort_phrases(self):
         '''Sorts phrases by length first (shortest first),
          and sum of word frequencies in phrase second
-         (highest sum first).'''
+         (highest sum first), and the phrase itself.'''
         weights = []
-        for idx,phrase in enumerate(self.phrases):
+        for idx, phrase in enumerate(self.phrases):
             weights.append(
                 (idx,
-                len(phrase),
-                sum([self.counter[word] for word in phrase]))
+                 len(phrase[0]),
+                 sum([self.counter[word] for word in phrase[0]]),
+                 phrase[0],)
             )
         # In the lambda expression,
         # minus implies reverse -> highest first
-        self.weights = sorted(weights, key=lambda x: (x[1], -x[2]))
+        self.weights = sorted(weights, key=lambda x: (x[1], -x[2], x[3]))
 
-    def write_partitions(self,output):
-        for word,_ in self.counter.most_common():
-            for phrase in self.phrases:
-                if word in phrase:
-                    output.write(word+","+' '.join(phrase)+','+"\n")
-            output.write('_'*80+'\n')
+    def write_sorted(self):
+        with open(self.sorted_path, 'w') as sortFile:
+            for weight in self.weights:
+                sortFile.write(
+                    str(' '.join(self.phrases[weight[0]][0])) + ','
+                    + self.phrases[weight[0]][1] + "\n"
+                )
 
-    def __add__(self,other):
-        print('self.phrases len:',len(self.phrases))
-        print('other.phrases len:',len(other.phrases))
-        self.phrases.extend(other.phrases)
-        print('After extending, self.phrases len:',len(self.phrases))
-        self.count_words()
-        self.sort_phrases()
-        return self
+    def write_counts(self):
+        with open(self.counts_path, 'w') as countsFile:
+            for word, freq in self.counter.most_common():
+                countsFile.write(word+','+str(freq)+'\n')
+
+    def write_partitions(self):
+        with open(self.partitions_path, 'w') as PartitionsFile:
+            for word, _ in self.counter.most_common():
+                lst = []
+                for phrase in self.phrases:
+                    if word in phrase[0]:
+                        lst.append([word,phrase[0],phrase[1]])
+                lst = sorted(lst,key=lambda x: x[1])
+                for line in lst:
+                    PartitionsFile.write(
+                        line[0] + ','
+                        + ' '.join(line[1]) + ','
+                        + line[2] + "\n")
+                PartitionsFile.write('_'*80+'\n')
+
 
 if __name__ == "__main__":
     file_names = [Path(item).stem for item in glob.glob('./config/*.csv')]
     print(file_names)
-    print('Processing',len(file_names),'files.')
-
-    for app_name in file_names:
-        sourcepath = f'./config/{app_name}.csv'
-        counts_path = f'./analytics/{app_name}_counts.csv'
-        partitions_path = f'./analytics/{app_name}_partitions.csv'
-
-        with open(sourcepath,'r') as inputFile:
-            analyzer = Analyze([inputFile])
-
-        with open(counts_path,'w') as countsFile:
-            for word,freq in analyzer.counter.most_common():
-                countsFile.write(word+','+str(freq)+'\n')
-
-        with open(partitions_path,'w') as partitionsFile:
-            analyzer.write_partitions(partitionsFile)
-
-    files = []
-    for app_name in file_names:
-        sourcepath = f'./config/{app_name}.csv'
-        files.append(open(sourcepath,'r'))
-
-    summary = Analyze(files)
-
-    counts_path = './analytics/counts_summary.csv'
-    with open(counts_path,'w') as countsFile:
-        for word,freq in summary.counter.most_common():
-            countsFile.write(word+','+str(freq)+'\n')
-
-    partitions_path = './analytics/partitions_summary.csv'
-    with open(partitions_path,'w') as partitionsFile:
-        summary.write_partitions(partitionsFile)
-
-    for f in files:
-        f.close()
+    print('Processing', len(file_names), 'files.')
+    _ = [AnalyzeCSV([name]) for name in file_names]
+    AnalyzeCSV(file_names)
