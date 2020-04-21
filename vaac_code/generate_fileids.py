f262b3ea9d99b8b2b50d98488cb75a7c3bbc591d
Shrinidhi <shrinidhihr98@gmail.com>
2020-04-21 20:46:21 +0530

    corrected fileids generator

--- vaac_code/generate_fileids.py
+++ vaac_code/generate_fileids.py
@@ -12,25 +12,24 @@ modeldir = 'vaac_model'
 test_fileids = modeldir+"/etc/"+modeldir+"_test.fileids"
 test_transcription = modeldir+"/etc/"+modeldir+"_test.transcription"
 words = sorted(glob.glob('./recordings/words/*'))
-for word in words[:1]:
-    with open(test_fileids,'w') as test_fileids_file, open(test_transcription,'w') as test_transcription_file:
-        for file in sorted(glob.glob(f'{word}/*')):
-            # To remove .wav extention
-            fileid = file.replace('./recordings/','')[:-4] +'\n'
-            transcription = f'<s> {Path(word).stem.upper()} </s> ({Path(file).stem})\n'
-            test_fileids_file.write(fileid)
-            test_transcription_file.write(transcription)
 
 train_fileids = modeldir+"/etc/"+modeldir+"_train.fileids"
 train_transcription = modeldir+"/etc/"+modeldir+"_train.transcription"
-for word in words[1:]:
-    with open(train_transcription,'a') as transcriptionfile, open (train_fileids,'a') as fileidsfile:        
+first = True
+for word in words:
+    with open(train_transcription,'a') as train_transcription_file, open (train_fileids,'a') as train_fileids_file, open(test_fileids,'a') as test_fileids_file, open(test_transcription,'a') as test_transcription_file:        
         for file in sorted(glob.glob(f'{word}/*')):
             # To remove .wav extention
             fileid = file.replace('./recordings/','')[:-4] +'\n'
             transcription = f'<s> {Path(word).stem.upper()} </s> ({Path(file).stem})\n'
-            fileidsfile.write(fileid)
-            transcriptionfile.write(transcription)
+            if first:
+                test_fileids_file.write(fileid)
+                test_transcription_file.write(transcription)
+            else:
+                train_fileids_file.write(fileid)
+                train_transcription_file.write(transcription)
+            first = False
+
 
 folders = sorted(glob.glob('./recordings/corpus/*'))
 for folder in folders:
