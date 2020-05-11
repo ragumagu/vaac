bede09e0520217d5f00f0941e555941e3a90194f
Shrinidhi <shrinidhihr98@gmail.com>
2020-05-11 20:37:23 +0530

    corrections to analyzeCorpus

--- analyzeCorpus.py
+++ analyzeCorpus.py
@@ -2,20 +2,21 @@ import csv
 import configparser
 from collections import Counter
 import glob
-from pathlib import Path
+
 
 class analyseCorpus:
-    def __init__(self,min_word_freq,max_word_freq):
+    def __init__(self, min_word_freq, max_word_freq):
         self.min_word_freq = min_word_freq
         self.max_word_freq = max_word_freq
         file_paths = glob.glob('corpus/*')
         print(file_paths)
         self.phrases = []
         for path in file_paths:
-            with open(path,'r') as corpus_file:
+            with open(path, 'r') as corpus_file:
                 self.phrases.extend(
-                    [phrase[0].split() for phrase in list(csv.reader(corpus_file))])
-        print('Total corpus length:',len(self.phrases))
+                    [phrase[0].split() for phrase in list(
+                        csv.reader(corpus_file))])
+        print('Total corpus length:', len(self.phrases))
         self.count_words()
         self.write_counts()
         self.cover_config()
@@ -29,13 +30,14 @@ class analyseCorpus:
                 self.counter[word] += 1
 
     def write_counts(self):
-        with open('./analytics/corpus_counts.csv','w') as corpus_counts:
+        with open('./analytics/corpus_counts.csv', 'w') as corpus_counts:
             for word, freq in self.counter.most_common()[::-1]:
                 corpus_counts.write(word+','+str(freq)+'\n')
 
     def cover_config(self):
-        with open('./analytics/counts_summary.csv','r') as config_counts_file:
-            config_counts = [item[0] for item in list(csv.reader(config_counts_file))]
+        with open('./analytics/counts_summary.csv', 'r') as config_counts_file:
+            config_counts = [item[0]
+                             for item in list(csv.reader(config_counts_file))]
 
         not_covered_list = []
         for word in config_counts:
@@ -46,33 +48,34 @@ class analyseCorpus:
             return
         print("List of words in config not in corpus:")
         for word in not_covered_list:
-                print(word)
+            print(word)
 
     def check_counts(self):
 
         imbalanced = []
         for word, freq in self.counter.most_common():
             if freq < self.min_word_freq:
-                imbalanced.append(['lower:',word,freq])
+                imbalanced.append(['lower:', word, freq])
             if freq > self.max_word_freq:
-                imbalanced.append(['higher:',word,freq])
+                imbalanced.append(['higher:', word, freq])
 
         if imbalanced == []:
             return
 
         print('List of words with imbalanced frequencies:')
         print('Words with frequencies lower and higher than')
-        print('Minimum word frequency:',self.min_word_freq)
-        print('Maximum word frequency:',self.max_word_freq)
+        print('Minimum word frequency:', self.min_word_freq)
+        print('Maximum word frequency:', self.max_word_freq)
         print('are tagged lower and higher.')
         for item in imbalanced:
             print(item)
+
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('./config/vaac_config')
 
-    min_word_freq = config.getint('CORPUS','min_word_freq')
-    max_word_freq = config.getint('CORPUS','max_word_freq')
+    min_word_freq = config.getint('CORPUS', 'min_word_freq')
+    max_word_freq = config.getint('CORPUS', 'max_word_freq')
 
-    analyseCorpus(min_word_freq,max_word_freq)
+    analyseCorpus(min_word_freq, max_word_freq)
