d40873a91d7171140c1bbd71e9814903f2c552d3
Shrinidhi <shrinidhihr98@gmail.com>
2020-05-11 22:04:15 +0530

    removed dead code

--- vaac_code/recorder.py
+++ vaac_code/recorder.py
@@ -1,21 +1,25 @@
-'''This is the vaac recorder module. It has a recordings manager which keeps track of corpus recording progress.
+'''This is the vaac recorder module. It has a recordings manager which keeps
+track of corpus recording progress.
 '''
 import csv
-import json
 import os
-import re
 import glob
 from pathlib import Path
 import subprocess
 import configparser
 
-# The _GetchUnix class replicates the functionality of the getch() method.
+
 class GetchUnix:
+    '''The _GetchUnix class replicates the functionality of the getch() method.
+    '''
     def __init__(self):
-        import tty, sys
+        import tty
+        import sys
 
     def __call__(self):
-        import sys, tty, termios
+        import sys
+        import tty
+        import termios
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
@@ -25,11 +29,12 @@ class GetchUnix:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
+
 class RecordingManager:
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('./config/vaac_config')
-        self.min_word_freq = config.getint('RECORDINGS','min_word_freq')
+        self.min_word_freq = config.getint('RECORDINGS', 'min_word_freq')
         self.min_command_freq = config.getint('RECORDINGS', 'min_command_freq')
 
         self.recording_words = True
@@ -39,13 +44,13 @@ class RecordingManager:
         self.i = 0
 
     def getWords(self):
-        with open('analytics/corpus_counts.csv','r') as wordsCountsFile:
+        with open('analytics/corpus_counts.csv', 'r') as wordsCountsFile:
             words = [item[0] for item in list(csv.reader(wordsCountsFile))]
 
         for word in words:
             for i in range(self.min_word_freq):
                 recpath = f'recordings/words/{word}/{word}_{i}.wav'
-                #print("DEBUG: searching for",recpath)
+
                 if not os.path.exists(recpath):
                     self.word = word
                     self.i = i
@@ -55,13 +60,13 @@ class RecordingManager:
         corpusfiles = sorted(glob.glob('./corpus/*'))
         for corpusfilestr in corpusfiles:
             corpusname = Path(corpusfilestr).stem
-            with open(corpusfilestr,'r') as corpusfile:
+            with open(corpusfilestr, 'r') as corpusfile:
                 corpus = list(csv.reader(corpusfile))
-            #print("DEBUG:getPhrases checking from",self.n, 'to', len(corpus))
-            for n in range(self.n,len(corpus)):
+
+            for n in range(self.n, len(corpus)):
                 for i in range(self.min_command_freq):
-                    recpath = f'recordings/corpus/{corpusname}/recording{n}_{i}.wav'
-                    #print("DEBUG: searching for",recpath)
+                    recpath = f'recordings/corpus/{corpusname}' + \
+                        f'/recording{n}_{i}.wav'
                     if not os.path.exists(recpath):
                         self.corpus = corpusname
                         self.n = n
@@ -71,7 +76,7 @@ class RecordingManager:
     def getNext(self):
         if self.recording_words:
             result = self.getWords()
-            if result is None: # has completed recording words
+            if result is None:  # has completed recording words
                 self.recording_words = False
                 return self.getPhrases()
             else:
@@ -86,7 +91,7 @@ class RecordingManager:
             recpath = f'recordings/words/{self.word}/{self.word}_{self.i}.wav'
         else:
             dirpath = f'recordings/corpus/{self.corpus}/'
-            recpath = f'recordings/corpus/{self.corpus}/recording{self.n}_{self.i}.wav'
-        subprocess.run(['mkdir','-p',dirpath])
-        subprocess.run(['mv','/tmp/test.wav',recpath])
-        print("DEBUG:moving to",recpath)
+            recpath = f'recordings/corpus/{self.corpus}/' + \
+                f'recording{self.n}_{self.i}.wav'
+        subprocess.run(['mkdir', '-p', dirpath])
+        subprocess.run(['mv', '/tmp/test.wav', recpath])
